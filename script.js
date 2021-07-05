9830cfcec00e79a1ae86b99280cb851cd99a34f7
ponlogeshp <ponlogeshvpt@gmail.com>
2021-07-05 12:36:06 +0530

    Massive Commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function computeInterest()
+	{
+		event.preventDefault();
+		var prinAmount=document.getElementById('pamount');
+		var rateOfInterest=document.getElementById('rateInterest');
+		var noOfYears=document.getElementById('nyears');
+		var interest=(prinAmount.value*noOfYears.value*rateOfInterest.value)/100;
+		var date=new Date();
+		var updatedYear=parseInt(date.getFullYear())+parseInt(noOfYears.value);
+		document.getElementById('compInterest').innerHTML="If you deposit <span bgcolor='yellow'><b>"+prinAmount.value+", </b></span>at an interest rate <span bgcolor='yellow'><b>"
+		+rateOfInterest.value+". </span></b><br>You will recieve an amount of <span bgcolor='yellow'><b>"+interest+" </b></span><br>in the year <span bgcolor='yellow'><b>"+updatedYear+" </b></span";
+	}
+function updateLabelInput(val)
+	{
+        document.getElementById('rangeInput').innerHTML=val; 
+    }
