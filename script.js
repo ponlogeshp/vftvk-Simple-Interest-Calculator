c4b55627ba6daf4989235bea2eb7eeeaf2bd72f3
ponlogeshp <ponlogeshvpt@gmail.com>
2021-07-05 13:28:52 +0530

    Updated Commit

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ function computeInterest()
 		var noOfYears=document.getElementById('nyears');
 		var interest=(prinAmount.value*noOfYears.value*rateOfInterest.value)/100;
 		var date=new Date();
+		if(prinAmount.value<=0)
+		{
+			alert("Principal should not be negative...Enter a positive value");
+			prinAmount.focus();
+		}
+		if(prinAmount.value=="")
+		{
+			alert("Empty values are not allowed..Enter a positive value");
+			prinAmount.focus();
+		}
 		var updatedYear=parseInt(date.getFullYear())+parseInt(noOfYears.value);
-		document.getElementById('compInterest').innerHTML="If you deposit <span bgcolor='yellow'><b>"+prinAmount.value+", </b></span>at an interest rate <span bgcolor='yellow'><b>"
-		+rateOfInterest.value+". </span></b><br>You will recieve an amount of <span bgcolor='yellow'><b>"+interest+" </b></span><br>in the year <span bgcolor='yellow'><b>"+updatedYear+" </b></span";
+		document.getElementById('compInterest').innerHTML="If you deposit <span style='background-color:yellow'><b>"+prinAmount.value+", </b></span>at an interest rate <span style='background-color:yellow'><b>"
+		+rateOfInterest.value+". </span></b><br>You will recieve an amount of <span style='background-color:yellow'><b>"+interest+" </b></span><br>in the year <span style='background-color:yellow'><b>"+updatedYear+" </b></span";
 	}
 function updateLabelInput(val)
 	{
